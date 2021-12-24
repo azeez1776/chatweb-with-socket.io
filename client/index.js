1c8bc34dcd6e2a88098734ed6ab54e49bf0ced9e
azeez1776 <aboud3am81@gmail.com>
2021-12-25 01:24:41 +0300

    made some changes

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const displayMessage = (message) => {
-    e.preventDefault();
+
     let x = document.querySelector('#message');
     // let message = x.value;
     // x.value = '';
@@ -17,7 +17,7 @@ const joinRoom = () => {
 
 const socket = io('http://localhost:3000')
 socket.on('connect', () => {
-    displayMessage(`You connected with id ${socket.id}`)
+    displayMessage(`You connected with id: ${socket.id}`)
 })
 
 document.querySelector('#send-message').addEventListener('click', displayMessage);
