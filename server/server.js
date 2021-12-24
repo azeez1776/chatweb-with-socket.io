4b963c9ec6992561db31d55d10de7c0dabe6dc24
azeez1776 <aboud3am81@gmail.com>
2021-12-25 00:57:21 +0300

    added the cors option on the server with socket.io

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
-const io = require('socket.io')(3000);
+const io = require('socket.io')(3000, {
+    cors: {
+        origin: ['https://localhost:8080']
+    }
+});
 
 io.on('connection', socket => {
     console.log(socket.id)
