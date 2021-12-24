1b2ac89177783834914093454c21fdbda6f825e7
azeez1776 <aboud3am81@gmail.com>
2021-12-25 01:48:05 +0300

    again formated the displayMessage func and added socket.emit

--- client/index.js
+++ client/index.js
@@ -3,8 +3,8 @@ import { io } from 'socket.io-client';
 const displayMessage = (message) => {
 
     let x = document.querySelector('#message');
-    // let message = x.value;
-    // x.value = '';
+    let message = message | x.value;
+    x.value = '';
     const div = document.createElement('div');
     div.textContent = message;
     return document.querySelector('#message-container').append(div)
@@ -18,6 +18,7 @@ const joinRoom = () => {
 const socket = io('http://localhost:3000')
 socket.on('connect', () => {
     displayMessage(`You connected with id: ${socket.id}`)
+    socket.emit('send-message', displayMessage())
 })
 
 document.querySelector('#send-message').addEventListener('click', displayMessage);
