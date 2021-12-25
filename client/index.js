f5c94365f1940919a990b30b49f1de25ec24a307
azeez1776 <aboud3am81@gmail.com>
2021-12-25 16:15:07 +0300

    again updated the displayMessage function

--- client/index.js
+++ client/index.js
@@ -2,9 +2,9 @@ import { io } from 'socket.io-client';
 
 const displayMessage = (message) => {
 
-    let x = document.querySelector('#message');
-    let message = message | x.value;
-    x.value = '';
+    // let x = document.querySelector('#message');
+    // let message = msg | x.value;
+    // x.value = '';
     const div = document.createElement('div');
     div.textContent = message;
     return document.querySelector('#message-container').append(div)
@@ -19,6 +19,10 @@ const socket = io('http://localhost:3000')
 socket.on('connect', () => {
     displayMessage(`You connected with id: ${socket.id}`)
     socket.emit('send-message', displayMessage())
+})
+
+socket.on('recieve-message', message => {
+    displayMessage(message)
 })
 
 document.querySelector('#send-message').addEventListener('click', displayMessage);
